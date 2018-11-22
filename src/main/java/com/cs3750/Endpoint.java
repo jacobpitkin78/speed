45edff37de219aa3c920dd42ac9e57bf066bd684
Jacob Pitkin <jacobpitkin@mail.weber.edu>
2018-11-21 19:44:30 -0700

    Added MessageFactory, as well as a couple more lines of code for the Endpoint

--- src/main/java/com/cs3750/Endpoint.java
+++ src/main/java/com/cs3750/Endpoint.java
@@ -9,20 +9,34 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import com.cs3750.game.*;
+import com.cs3750.messages.*;
+
 @ServerEndpoint("/game")
 public class Endpoint {
 	private ArrayList<Session> connections = new ArrayList<Session>();
     
     @OnOpen
     public void onOpen(Session session) {
-        System.out.println("onOpen::" + session.getId());    
-        connections.add(session);
+        System.out.println("onOpen::" + session.getId()); 
+        
+        if (connections.size() < 2) {
+        	connections.add(session);
+        }
     }
     
     @OnClose
     public void onClose(Session session) {
         System.out.println("onClose::" +  session.getId());
         connections.remove(session);
+        
+        if (!connections.isEmpty()) {
+        	try {
+				connections.get(0).getBasicRemote().sendText(MessageFactory.getGameMessage("Congratulations, you won due to a forfeit!"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        }
     }
     
     @OnMessage
---
+++ src/main/java/com/cs3750/messages/MessageFactory.java
@@ -0,0 +1,209 @@
+package com.cs3750.messages;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+
+public class MessageFactory {
+	
+	public String getConnectMessage(ConnectMessage connectMessage) {
+		return getConnectMessage(connectMessage.getUsername());
+	}
+	
+	public String getConnectMessage(String username) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "connect");
+		objBuilder.add("username", username);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getDrawMessage(DrawMessage drawMessage) {
+		return getDrawMessage();
+	}
+	
+	public static String getDrawMessage() {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "draw");
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getMoveMessage(MoveMessage moveMessage) {
+		return getMoveMessage(moveMessage.getFrom(), moveMessage.getTo());
+	}
+	
+	public static String getMoveMessage(int from, int to) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "move");
+		objBuilder.add("from", from);
+		objBuilder.add("to", to);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getCardMessage(CardMessage cardMessage) {
+		return getCardMessage(cardMessage.getCard());
+	}
+	
+	public static String getCardMessage(int card) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "card");
+		objBuilder.add("card", card);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getChatMessage(ChatMessage chatMessage) {
+		return getChatMessage(chatMessage.getMessage(), chatMessage.getUsername());
+	}
+	
+	public static String getChatMessage(String message, String username) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "chat");
+		objBuilder.add("username", username);
+		objBuilder.add("message", message);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getGameMessage(GameMessage gameMessage) {
+		return getGameMessage(gameMessage.getMessage());
+	}
+	
+	public static String getGameMessage(String message) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "game");
+		objBuilder.add("message", message);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getInvalidMessage(InvalidMessage invalidMessage) {
+		return getInvalidMessage(invalidMessage.getMessage());
+	}
+	
+	public static String getInvalidMessage(String message) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "invalid");
+		objBuilder.add("message", message);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public String getMiddleCards(MiddleCards middleCards) {
+		return getMiddleCards(middleCards.getCards());
+	}
+	
+	public static String getMiddleCards(List<Integer> cards) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "middle");
+		
+		JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
+		
+		for (int i : cards) {
+			arrBuilder.add(Json.createObjectBuilder().add("card", i));
+		}
+		
+		objBuilder.add("cards", arrBuilder);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getOpponentCards(OpponentCards opponentCards) {
+		return getOpponentCards(opponentCards.getCards());
+	}
+	
+	public static String getOpponentCards(List<Integer> cards) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "opponent");
+		objBuilder.add("cards", cards.size());
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getPlayerCards(PlayerCards playerCards) {
+		return getPlayerCards(playerCards.getCards());
+	}
+	
+	public static String getPlayerCards(List<Integer> cards) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "player");
+		
+		JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
+		
+		for (int i : cards) {
+			arrBuilder.add(Json.createObjectBuilder().add("card", i));
+		}
+		
+		objBuilder.add("cards", arrBuilder);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static String getStartMessage(StartMessage startMessage) {
+		return getStartMessage(startMessage.getUsername());
+	}
+	
+	public static String getStartMessage(String username) {
+		JsonObjectBuilder objBuilder = Json.createObjectBuilder();
+		objBuilder.add("type", "start");
+		objBuilder.add("username", username);
+		
+		return objBuilder.build().toString();
+	}
+	
+	public static Message parse(String json) {
+		JsonReader reader = Json.createReader(new StringReader(json));
+    	String type = reader.readObject().getString("type");
+    	
+    	if (type.equals("ack")) {
+    		return new AckMessage();
+    	} else if (type.equals("card")) {
+    		return new CardMessage(reader.readObject().getInt("card"));
+    	} else if (type.equals("chat")) {
+    		return new ChatMessage(reader.readObject().getString("message"), reader.readObject().getString("username"));
+    	} else if (type.equals("complementHandCards")) {
+    		// Need to build function
+    	} else if (type.equals("connect")) {
+    		return new ConnectMessage(reader.readObject().getString("username"));
+    	} else if (type.equals("draw")) {
+    		return new DrawMessage();
+    	} else if (type.equals("game")) {
+    		return new GameMessage(reader.readObject().getString("message"));
+    	} else if (type.equals("invalid")) {
+    		return new InvalidMessage(reader.readObject().getString("message"));
+    	} else if (type.equals("move")) {
+    		return new MoveMessage(reader.readObject().getString("username"), reader.readObject().getInt("from"), reader.readObject().getInt("to"));
+    	} else if (type.equals("opponentCards")) {
+    		List<Integer> cards = new ArrayList<Integer>();
+    		
+    		for (int i = 0; i < reader.readArray().size(); i++) {
+    			cards.add(reader.readArray().getInt(i));
+    		}
+    		
+    		return new OpponentCards(cards);
+    	} else if (type.equals("playerCards")) {
+    		List<Integer> cards = new ArrayList<Integer>();
+    		
+    		for (int i = 0; i < reader.readArray().size(); i++) {
+    			cards.add(reader.readArray().getInt(i));
+    		}
+    		
+    		return new PlayerCards(cards);
+    	} else if (type.equals("result")) {
+    		
+    	} else if (type.equals("start")) {
+    		return new StartMessage();
+    	} else {
+    		// Probably throw error, since no other message exists
+    	}
+    	
+    	return null;
+	}
+}
